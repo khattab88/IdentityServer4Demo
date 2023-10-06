a1f2076358c66f79ee5e4c0a24ffb993edcf1518
khattab88 <m_khatab_88@hotmail.com>
2023-10-07 01:40:32 +0300

    consume protected api with access token

--- Movies.Client/Services/MovieService.cs
+++ Movies.Client/Services/MovieService.cs
@@ -1,12 +1,66 @@
-﻿using Movies.Client.Models;
+﻿using IdentityModel.Client;
+using Movies.Client.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Movies.Client.Services
 {
     public class MovieService : IMovieService
     {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            // 1. get token from identity server
+            // 2. send request to protected api
+            // 3. deserialize response 
+
+            // 1.1 retrieve our client credientials
+            var apiClientCredentials = new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:5005/connect/token",
+                ClientId = "movieClient",
+                ClientSecret = "secret",
+                Scope = "movieAPI"
+            };
+
+            var client = new HttpClient();
+
+            // 1.2 check if identity server discovery document is available
+            var discov = await client.GetDiscoveryDocumentAsync("https://localhost:5005");
+            if (discov.IsError)
+            {
+                return null; // throw 500 error
+            }
+
+            // 1.3 authenticate and get access token 
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            if (tokenResponse.IsError)
+            {
+                return null;
+            }
+
+
+            // 2.1 create another http client for api
+            var apiClient = new HttpClient();
+
+            // 2.2 set access_token in request Authorization: Bearer <token>
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            // 2.3 send request to api
+            var apiResponse = await apiClient.GetAsync("https://localhost:5001/api/movies");
+            apiResponse.EnsureSuccessStatusCode();
+
+            var content = await apiResponse.Content.ReadAsStringAsync();
+
+
+            // 3.1 deserialize response content
+            List<Movie> movies = JsonConvert.DeserializeObject<List<Movie>>(content);
+            return movies;
+        }
+
+
         public Task<Movie> CreateMovie(Movie movie)
         {
             throw new System.NotImplementedException();
@@ -21,36 +75,7 @@ namespace Movies.Client.Services
         {
             throw new System.NotImplementedException();
         }
-
-        public async Task<IEnumerable<Movie>> GetMovies()
-        {
-            var movies = new List<Movie>() 
-            {
-                new Movie
-                {
-                    Id = 1,
-                    Title = "Test 1",
-                    Genre = "Test 1",
-                    ImageUrl = null,
-                    Owner = null,
-                    Rating = null,
-                    ReleaseDate = DateTime.Now
-                },
-                new Movie
-                {
-                    Id = 2,
-                    Title = "Test 2",
-                    Genre = "Test 2",
-                    ImageUrl = null,
-                    Owner = null,
-                    Rating = null,
-                    ReleaseDate = DateTime.Now
-                }
-            };  
-
-            return await Task.FromResult(movies);
-        }
-
+        
         public Task<Movie> UpdateMovie(Movie movie)
         {
             throw new System.NotImplementedException();
